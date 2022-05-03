49381d5a5d3425bec912268bb6c832349f7efa13
FIDEL DE LA ROCHA <chindacuil.45@gmail.com>
2022-05-02 20:04:14 -0500

    iniciando la aplicacion

--- Servidor.js
+++ Servidor.js
@@ -1,2 +1,14 @@
+var http =require('http');
+var fs = require ('fs');
+var path = require('path');
 
+http.createServer(function(request,response){
+console.log('request', request.url);
+
+var filePath ='.'+ request.url;
+if(filePath=='./'){
+
+ filePath ='./index.html';
+
+}
 
